f7db017f245e6212ddedcc7b54090af03be9b46c
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-08-11 15:26:16 +0200

    добавляет дз 10

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './styles.css';
+import menu from './menu.json';
+import itemTemplate from './templates/item.hbs';
+import allItemsTemplate from './templates/allitems.hbs';
+const menuList = document.querySelector('.js-menu');
+
+//*Генерация каждого элемента отдельно
+
+// const item = itemTemplate(menu);
+// menuList.insertAdjacentHTML('afterbegin', item);
+
+//*Генерация всех элементов сразу
+
+function generateMenu(menu) {
+  const item = menu.map(menuItem => allItemsTemplate(menuItem)).join('');
+  menuList.insertAdjacentHTML('afterbegin', item);
+}
+generateMenu(menu);
+
+//* Переключение темы
+
+const checkBox = document.querySelector('.js-switch-input');
+const body = document.querySelector('body');
+
+checkBox.addEventListener('click', () => {
+  if (checkBox.checked) {
+    body.classList.add('dark-theme');
+    localStorage.setItem('isChecked', true);
+  } else {
+    body.classList.remove('dark-theme');
+    localStorage.setItem('isChecked', false);
+  }
+});
+
+checkBox.checked = localStorage.getItem('isChecked') === 'true';
+checkBox.checked
+  ? body.classList.add('dark-theme')
+  : body.classList.remove('dark-theme');
